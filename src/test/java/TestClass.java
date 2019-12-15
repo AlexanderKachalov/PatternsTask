304f2c7694f6877435ec2ae17581e4c83daebbf0
Alexander Kachalov <kachalow77@gmail.com>
2019-12-16 00:17:52 +0300

    updated TestClass.java added

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -10,28 +10,31 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestClass {
-    private Faker faker = new Faker(new Locale("ru"));;
+//    private Faker faker = new Faker(new Locale("ru"));
     private SelenideElement form;
-    private LocalDate date = LocalDate.now();
-    private int day = date.getDayOfMonth();
-    private String nameUser = faker.name().firstName();
-    private String phoneNumber = faker.phoneNumber().phoneNumber().replaceAll("[()\\-]", "");
-    private String cityName = faker.address().city();
+    LocalDate date = LocalDate.now();
+    int day = date.getDayOfMonth();
+//    private LocalDate date = LocalDate.now();
+//    private int day = date.getDayOfMonth();
+//    private String nameUser = faker.name().firstName();
+//    private String phoneNumber = faker.phoneNumber().phoneNumber().replaceAll("[()\\-]", "");
+//    private String cityName = faker.address().city();
 
     @Test
     void testFormIfValidateAllInputData() {
         openForm();
-        inputCityName(cityName);
-        inputNameUser(nameUser);
-        inputPhoneNumber(phoneNumber);
-        inputData();
+//        inputCityName(cityName);
+//        inputNameUser(nameUser);
+//        inputPhoneNumber(phoneNumber);
+//        inputData();
+        inputValidData();
         form.$("[data-test-id=agreement]").click();
         form.$$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=success-notification]").waitUntil(visible, 5000);
+//        $("[data-test-id=success-notification]").waitUntil(visible, 5000);
         $(By.xpath("//*[@id='root']/div/div[1]/button")).click();
         inputOtherData();
         form.$$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=replan-notification]").waitUntil(visible, 5000);
+//        $("[data-test-id=replan-notification]").waitUntil(visible, 5000);
         form.$$("button").find(exactText("Перепланировать")).click();
         $("[data-test-id=success-notification]").waitUntil(visible, 5000);
     }
@@ -41,23 +44,35 @@ public class TestClass {
         form = $(By.className("App_appContainer__3jRx1"));
     }
 
-    private void inputCityName (String cityName) {
+    private void inputValidData() {
+        Faker faker = new Faker(new Locale("ru"));
+        String nameUser = faker.name().firstName();
+        String phoneNumber = faker.phoneNumber().phoneNumber().replaceAll("[()\\-]", "");
+        String cityName = faker.address().city();
         form.$("[data-test-id=city] input").setValue(cityName);
-    }
-
-    private void inputNameUser (String nameUser) {
         form.$("[data-test-id=name] input").setValue(nameUser);
-    }
-
-    private void inputPhoneNumber (String phoneNumber) {
         form.$("[data-test-id=phone] input").setValue(phoneNumber);
-    }
-
-    private void inputData () {
         form.$("[data-test-id=date]").click();
         form.$(By.xpath("//*[text()='" + (day + 5) + "']")).click();
     }
 
+//    private void inputCityName (String cityName) {
+//        form.$("[data-test-id=city] input").setValue(cityName);
+//    }
+//
+//    private void inputNameUser (String nameUser) {
+//        form.$("[data-test-id=name] input").setValue(nameUser);
+//    }
+//
+//    private void inputPhoneNumber (String phoneNumber) {
+//        form.$("[data-test-id=phone] input").setValue(phoneNumber);
+//    }
+//
+//    private void inputData () {
+//        form.$("[data-test-id=date]").click();
+//        form.$(By.xpath("//*[text()='" + (day + 5) + "']")).click();
+//    }
+//
     private void inputOtherData () {
         form.$("[data-test-id=date]").click();
         form.$(By.xpath("//*[text()='" + (day + 4) + "']")).click();
